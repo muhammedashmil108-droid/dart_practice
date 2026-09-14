494c439749dbcfaa4bebece0de0d913c8680ee8f
Muhammed Ashmil <muhammedashmil108@gmail.com>
2026-09-14 09:51:00 +0530

    question1

--- day_1.dart
+++ day_1.dart
@@ -1,12 +1,24 @@
 void main() {
 question1();
 
-print("positive num");
-print("zero");
 
 
 
 }
 question1(){
-
+List<int> numbers = [13,54,63,34,65,23,12,45,67,89];
+int sum = 0;
+int largest = numbers[0];
+int secondlargest = numbers[0];
+for(int i=0; i<numbers.length; i++){
+  sum +=numbers[i];
+  if(numbers[i]>largest){
+    secondlargest=largest;
+    largest=numbers[i];
+}else if(numbers[i]>secondlargest){
+  secondlargest=numbers[i];
+}
+}
+print("The largest number is: $largest");
+print("The second largest number is: $secondlargest");
 }
