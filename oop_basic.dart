3edae5e37b4f253d1587d8dbfa106f3f43c2c7bd
Muhammed Ashmil <muhammedashmil108@gmail.com>
2026-09-15 11:46:59 +0530

    dart day3 2

--- oop_basic.dart
+++ oop_basic.dart
@@ -1,19 +1,19 @@
 
-class student {
-    String name="ashmil";
-    int age=18;
+// class student {
+//     String name="ashmil";
+//     int age=18;
 
-    void study(){
-        print("$name is currently studying");
-    }
-}
+//     void study(){
+//         print("$name is currently studying");
+//     }
+// }
 
-void main(){
-    student student1=student();
-    print("Name: ${student1.name}");
-    print("Age: ${student1.age}");
-    student1.study();   
-}
+// void main(){
+//     student student1=student();
+//     print("Name: ${student1.name}");
+//     print("Age: ${student1.age}");
+//     student1.study();   
+// }
 
 // class std {
 //     String? name;
@@ -31,3 +31,16 @@ void main(){
 //     print("name: ${s2.name}, age: ${s2.age}");
 //     print("name: ${s3.name}, age: ${s3.age}");  
 // }
+
+
+class student {
+    String? name;
+    int? age;
+
+    student(this.name,this.age);
+}
+void main(){
+    student std=student("ashmil",18);
+    print(std.name);
+    print(std.age);
+}
